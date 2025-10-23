596d666d8e01d7f4e55ffd797cd4a45829d03fb0
Your Name <pdanghuy03@gmail.com>
2025-10-23 21:43:05 +0800

    ft(run): add run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Implement me!"
+set -e
+
+echo "Building and starting all services..."
+
+docker compose up --build -d
+
+echo "-----------------------------------------------"
+echo "Services are up and running."
+echo "API will be available at http://localhost:8080"
+echo "Following logs (press Ctrl+C to stop following):"
+echo "-----------------------------------------------"
+
+docker compose logs -f
