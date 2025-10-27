a1adc2bdeade737ebfe927b92c196ea81cb99995
Bryan Dang <82434977+DKER2@users.noreply.github.com>
2025-10-27 09:54:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,3 @@
-set -e
-
-# Function to clean up containers on exit
-cleanup() {
-    echo -e "\nStopping all services..."
-    docker compose down
-}
-trap cleanup SIGINT
-
 echo "Building and starting all services..."
 
-docker compose up --build -d
-
-echo "-----------------------------------------------"
-echo "Services are up and running."
-echo "API will be available at http://localhost:8080"
-echo "Following logs (press Ctrl+C to stop following and shut down services):"
-echo "-----------------------------------------------"
-
-docker compose logs -f
+docker compose up --build 
